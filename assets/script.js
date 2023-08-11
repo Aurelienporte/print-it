bddfccb1c189d6b08156b15d298d2eed442dee01
Aurélien Porté <aurel.porte@gmail.com>
2023-08-11 19:42:38 +0200

    js sript du défliment du carrousel. css suppression d'une règle vide

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,50 @@ const fleches = document.querySelectorAll('.arrow');
 function info(){
 	console.log(this);
 }
+
+// faire défiler les diapos
+const image = document.querySelector('.banner-img');
+const rangeeDePoints = document.querySelectorAll('.dot');
+const texteBanniere = document.querySelector('#banner p');
+
+let numeroDiapo = 0;
+
+const chevronDroit = document.querySelector('.arrow_right');
+chevronDroit.addEventListener('click', avancer);
+const chevronGauche = document.querySelector('.arrow_left');
+chevronGauche.addEventListener('click', reculer);
+
+function avancer(){
+	numeroDiapo++;
+	if(numeroDiapo>3){
+		numeroDiapo=0;
+	}
+	diapoActive();
+	texteActif();
+	pointActif();
+}
+
+function reculer(){
+	numeroDiapo--;
+	if(numeroDiapo<0){
+		numeroDiapo=3;
+	}
+	diapoActive();
+	texteActif();
+	pointActif();
+}
+
+function diapoActive(){
+	image.setAttribute('src', './assets/images/slideshow/' + slides[numeroDiapo].image);	
+}
+
+function pointActif(){
+	for(point of rangeeDePoints){
+		point.classList.remove('dot_selected');
+	}
+	rangeeDePoints[numeroDiapo].classList.add('dot_selected');
+}
+
+function texteActif(){
+	texteBanniere.innerHTML = slides[numeroDiapo].tagLine;
+}
